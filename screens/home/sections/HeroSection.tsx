94d99fcf043b6a938b7d5f202f6fe6fdaf89a074
Ryland Crouse <rmikhailc@gmail.com>
2023-01-13 14:34:40 -0500

    adjusted and animated hero section

--- screens/home/sections/HeroSection.tsx
+++ screens/home/sections/HeroSection.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 const HeroSection = (props: Props) => {
   return (
-    <div className="h-screen w-full flex 2xl:px-72 2xl:justify-start px-th justify-center font-bold bg-black text-neutral-100">
-      <h1 className="self-center justify-self-center font-bold font-psans !leading-tight 
-      xl:text-9xl lg:w-[1300px] text-[3vmax]">“Howdy 👋🏻. I leverage and expand upon digital solutions with code.”</h1>
+    <div className="h-screen w-full flex 2xl:px-72 2xl:justify-start px-th justify-center font-bold">
+      <h1 className="self-center justify-self-center font-semibold !leading-tight 
+      xl:text-9xl lg:w-[1300px] text-[3vmax]">“Howdy <span className="blog-title-emoji">👋🏻</span>. I leverage and expand upon digital solutions with code.”</h1>
      </div>
   )
 }
