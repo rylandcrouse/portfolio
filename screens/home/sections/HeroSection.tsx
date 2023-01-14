75bf653452880e9b566f9b17a50c6dd2c29a5c42
Ryland Crouse <rmikhailc@gmail.com>
2023-01-14 00:18:01 -0500

    changed styling

--- screens/home/sections/HeroSection.tsx
+++ screens/home/sections/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = (props: Props) => {
   return (
     <div className="h-screen w-full flex 2xl:px-72 2xl:justify-start px-th justify-center font-bold">
       <h1 className="self-center justify-self-center font-semibold !leading-tight 
-      xl:text-8xl lg:w-[1300px] sm:text-[5vmax] text-4xl">
+      xl:text-8xl sm:text-[4vmax] text-3xl">
         “Howdy <span className="blog-title-emoji">👋🏻</span>. I leverage and expand upon digital solutions with code.”
       </h1>
      </div>
