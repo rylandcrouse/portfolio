f538339aa9de793b77b827a9f183d068f14c29da
Ryland Crouse <rmikhailc@gmail.com>
2023-01-14 00:36:39 -0500

    styled footer

--- screens/home/HomeScreen.tsx
+++ screens/home/HomeScreen.tsx
@@ -8,7 +8,7 @@ type Props = {}
 
 const HomeScreen = (props: Props) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center antialiased">
+    <div className="flex min-h-screen flex-col items-center justify-center antialiased bg-neutral-900 text-neutral-100">
     <Head>
       <title>Ryland Crouse</title>
       <link rel="icon" href="/favicon.ico" />
@@ -19,15 +19,15 @@ const HomeScreen = (props: Props) => {
         <SampleSection/>
     </main>
 
-    <footer className="flex h-24 w-full items-center justify-center border-t">
+    <footer className="flex h-24 w-full items-center justify-center bg-black border-neutral-900 border-t">
       <a
-        className="flex items-center justify-center gap-2"
+        className="flex items-center justify-center gap-4"
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer"
       >
         Powered by{' '}
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        <Image src="/me.png" alt="Vercel Logo" width={32} height={16} />
       </a>
     </footer>
   </div>
