0677c6f08c36b2e318ba49495a857b3c9440dde7
Ryland Crouse <rmikhailc@gmail.com>
2023-01-14 11:33:56 -0500

    base text size up to xl bp

--- screens/home/sections/SampleSection.tsx
+++ screens/home/sections/SampleSection.tsx
@@ -8,7 +8,7 @@ const SampleSection = (props: Props) => {
     <div className="min-w-full h-full px-7 sm:px-0
         flex items-center justify-around sm:space-y-0 space-y-4
         flex-col sm:flex-row
-        text-xl sm:max-lg:text-base">
+        text-xl max-xl:text-base">
         <Sample title="Automate Zoning Code Search with ChatGPT" emoji="🎸" repoURL="github.com/rylandcrouse/"/>
         <Sample title="Fretboard Exersize" emoji="🎸" repoURL="github.com/rylandcrouse/portfolio"/>
         <Sample title="Automate Zoning Code Search with ChatGPT" emoji="🎸" repoURL="http://github.com/rylandcrouse/portfolio"/>
