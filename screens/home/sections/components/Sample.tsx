d52a097b7d0772eb9616bfbafd0145649e77431a
Ryland Crouse <rmikhailc@gmail.com>
2023-01-14 12:05:36 -0500

    single inactive border color

--- screens/home/sections/components/Sample.tsx
+++ screens/home/sections/components/Sample.tsx
@@ -13,7 +13,7 @@ const Sample = ({title, emoji, repoURL}: Props) => {
   return (
         <div className="
         flex flex-col justify-evenly items-center px-6 p-6
-        sm:w-1/3 w-full h-full sm:aspect-square aspect-[9/2] sm:border-neutral-900/30 border-neutral-900/40 sm:border-r border
+        sm:w-1/3 w-full h-full sm:aspect-square aspect-[9/2] border-neutral-900/30 sm:border-r border
         hover:cursor-pointer md:hover:bg-slate-900/60 lg:hover:scale-[101%] hover:ease-out duration-300
         font-normal bg-gradient-to-r from-zinc-900/30 to-zinc-800/20">
             <h2 className="text-center font-bold">{title}</h2>
