4c9c2100fc63f88b655265495f516fd3553035b0
Ryland Crouse <rmikhailc@gmail.com>
2023-01-14 03:53:47 -0500

    hero spacing

--- screens/home/sections/HeroSection.tsx
+++ screens/home/sections/HeroSection.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 const HeroSection = (props: Props) => {
   return (
-    <div className="min-h-[50vh] w-full flex pt-4 2xl:px-72 2xl:justify-start px-th justify-center font-bold">
+    <div className="min-h-[50vh] w-full flex md:pt-0 pt-4 2xl:px-72 2xl:justify-start px-th justify-center font-bold">
       <h1 className="self-center justify-self-center font-semibold !leading-tight 
-      xl:text-8xl sm:text-[4vmax] text-3xl lg:my-6">
+      xl:text-8xl sm:text-[4vmax] text-3xl lg:my-16">
         “Howdy <span className="blog-title-emoji">👋🏻</span>. I leverage and expand upon digital solutions with code.”
       </h1>
      </div>
