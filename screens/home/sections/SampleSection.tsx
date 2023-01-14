ba7025016b129b24da75e272057b5b80f2a43ba7
Ryland Crouse <rmikhailc@gmail.com>
2023-01-13 21:58:13 -0500

    hyperlinked work samples and changed styles

--- screens/home/sections/SampleSection.tsx
+++ screens/home/sections/SampleSection.tsx
@@ -5,16 +5,14 @@ type Props = {}
 
 const SampleSection = (props: Props) => {
   return (
-    <div className="px-[5%] lg:py-0 py-4 lg:min-h-[90vh] w-full
+    <div className="lg:px-[5%] lg:py-0 sm:py-4 lg:min-h-[90vh] w-full
         flex items-center
         flex-col lg:flex-row
-        space-x-0 lg:space-x-12 lg:space-y-0 space-y-5
-        text-2xl font-medium
-        ">
-            <Sample title="Automate Zoning Code Search with ChatGPT" emoji="🎸"/>
-            <Sample title="Practice Guitar Theory" emoji="🎸"/>
-            <Sample title="Practice Guitar Theory" emoji="🎸"/>
-            
+        space-x-0 lg:space-x-12 lg:space-y-0 md:space-y-5
+        text-2xl font-medium">
+        <Sample title="Automate Zoning Code Search with ChatGPT" emoji="🎸" repoURL="github.com/rylandcrouse/"/>
+        <Sample title="Automate Zoning Code Search with ChatGPT" emoji="🎸" repoURL="github.com/rylandcrouse/portfolio"/>
+        <Sample title="Automate Zoning Code Search with ChatGPT" emoji="🎸" repoURL="http://github.com/rylandcrouse/portfolio"/>
     </div>
   )
 }
--- screens/home/sections/components/Sample.tsx
+++ screens/home/sections/components/Sample.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react'
 import { AnimatePresence } from "framer-motion";
+import Link from 'next/link';
 
 type Props = {
     title: string;
     emoji: string;
+    repoURL: string;
 }
 
-const Sample = ({title, emoji}: Props) => {
+const Sample = ({title, emoji, repoURL}: Props) => {
     const [hovered, setHovered] = useState(false)
   return (
-    <div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="aspect-[7/8] max-h-[70vh] w-full min-w-1/3 lg:px-10 px-14 py-10 max-w-[700px]
-    flex flex-col justify-evenly items-center space-y-5
-    rounded-xl border-neutral-900/60 xl:border-none
-    hover:cursor-pointer hover:bg-slate-900/60 sm:hover:scale-105 hover:scale-none hover:ease-out duration-300
-    font-normal bg-gradient-to-r from-neutral-900/10 to-slate-900/10">
-        <h2 className="text-center font-medium">{title}</h2>
-        <AnimatePresence>
-            <div className={`guitar text-9xl ${hovered && "animate-prop"}`}>{emoji}</div>
-        </AnimatePresence>
-    </div>
+    <Link href={`${repoURL}`}>
+        <div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="sm:aspect-[7/8] aspect-square max-h-[70vh] w-full min-w-1/3 lg:px-10 px-14 py-10 max-w-[700px]
+        flex flex-col justify-evenly items-center space-y-5
+        rounded-xl border-neutral-900/60 xl:border-none
+        hover:cursor-pointer hover:bg-slate-900/60 lg:hover:scale-105 hover:scale-[101%] hover:scale-none hover:ease-out duration-300
+        font-normal bg-gradient-to-r from-neutral-900/10 to-slate-900/10">
+            <h2 className="text-center font-medium">{title}</h2>
+            <AnimatePresence>
+                <div className={`guitar text-9xl ${hovered && "animate-prop"}`}>{emoji}</div>
+            </AnimatePresence>
+        </div>
+    </Link>
   )
 }
 
