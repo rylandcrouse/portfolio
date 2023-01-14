2b8fa674fc726f7286b20d9d345e3d084f1170f0
Ryland Crouse <rmikhailc@gmail.com>
2023-01-14 03:15:08 -0500

    removed sample padding

--- screens/home/sections/SampleSection.tsx
+++ screens/home/sections/SampleSection.tsx
@@ -5,10 +5,10 @@ type Props = {}
 
 const SampleSection = (props: Props) => {
   return (
-    <div className="px-th lg:min-h-[90vh] min-w-full h-full
-        flex items-center justify-around lg:space-x-10 lg:space-y-0 space-y-6 space-x-0 sm:py-8 pb-4
-        flex-col lg:flex-row
-        text-xl font-medium">
+    <div className="min-w-full h-full px-7 sm:px-0
+        flex items-center justify-around sm:space-y-0 space-y-4
+        flex-col sm:flex-row
+        text-xl sm:max-lg:text-base font-medium">
         <Sample title="Automate Zoning Code Search with ChatGPT" emoji="🎸" repoURL="github.com/rylandcrouse/"/>
         <Sample title="Fretboard Exersize" emoji="🎸" repoURL="github.com/rylandcrouse/portfolio"/>
         <Sample title="Automate Zoning Code Search with ChatGPT" emoji="🎸" repoURL="http://github.com/rylandcrouse/portfolio"/>
--- screens/home/sections/components/Sample.tsx
+++ screens/home/sections/components/Sample.tsx
@@ -15,13 +15,12 @@ const Sample = ({title, emoji, repoURL}: Props) => {
         <div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}
         className="
         flex flex-col justify-evenly items-center px-6 p-6
-        lg:w-1/3 sm:w-1/2 w-full h-full sm:aspect-[9/10] aspect-[9/3] 
-        sm:rounded-xl sm:border-neutral-900/30 border border-neutral-900/50
-        hover:cursor-pointer sm:hover:bg-slate-900/60 lg:hover:scale-105 sm:hover:scale-[101%] hover:ease-out duration-300
+        sm:w-1/3 w-full h-full sm:aspect-square aspect-[9/2] sm:border-neutral-900/30 border-neutral-900/50 border-r
+        hover:cursor-pointer sm:hover:bg-slate-900/60 md:hover:scale-[101%] hover:ease-out duration-300
         font-normal bg-gradient-to-r from-neutral-900/10 to-slate-700/5">
             <h2 className="text-center font-black">{title}</h2>
             <AnimatePresence>
-                <div className={`hidden sm:block guitar text-8xl`}>{emoji}</div>
+                <div className={`hidden md:block guitar text-7xl xl:text-8xl 2xl:text-9xl`}>{emoji}</div>
             </AnimatePresence>
         </div>
     // </Link>
