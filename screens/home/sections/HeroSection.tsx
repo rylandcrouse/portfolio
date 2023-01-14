bc46806699abb48c8968a0c46ce6b8fde9d92fd2
Ryland Crouse <63918595+rylandcrouse@users.noreply.github.com>
2023-01-14 09:30:00 -0500

    Update HeroSection.tsx

--- screens/home/sections/HeroSection.tsx
+++ screens/home/sections/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = (props: Props) => {
   return (
     <div className="min-h-[50vh] w-full flex 2xl:px-72 2xl:justify-start px-th justify-center font-bold">
       <h1 className="self-center justify-self-center font-semibold !leading-tight 
-      xl:text-8xl sm:text-[4vmax] text-3xl lg:my-16 mt-10">
+      xl:text-8xl sm:text-[4vmax] text-3xl lg:my-16 mt-12">
         “Howdy <span className="blog-title-emoji">👋🏻</span>. I leverage and expand upon digital solutions with code.”
       </h1>
      </div>
