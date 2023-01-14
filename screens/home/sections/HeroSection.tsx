4efb07f1c6703badf22a3c2b6da5eb5c480a92d8
Ryland Crouse <rmikhailc@gmail.com>
2023-01-13 22:45:26 -0500

    changed styles

--- screens/home/sections/HeroSection.tsx
+++ screens/home/sections/HeroSection.tsx
@@ -6,7 +6,9 @@ const HeroSection = (props: Props) => {
   return (
     <div className="h-screen w-full flex 2xl:px-72 2xl:justify-start px-th justify-center font-bold">
       <h1 className="self-center justify-self-center font-semibold !leading-tight 
-      xl:text-8xl lg:w-[1300px] text-[5vmax]">“Howdy <span className="blog-title-emoji">👋🏻</span>. I leverage and expand upon digital solutions with code.”</h1>
+      xl:text-8xl lg:w-[1300px] sm:text-[5vmax] text-4xl">
+        “Howdy <span className="blog-title-emoji">👋🏻</span>. I leverage and expand upon digital solutions with code.”
+      </h1>
      </div>
   )
 }
--- screens/home/sections/components/Sample.tsx
+++ screens/home/sections/components/Sample.tsx
@@ -12,11 +12,11 @@ const Sample = ({title, emoji, repoURL}: Props) => {
     const [hovered, setHovered] = useState(false)
   return (
     <Link href={`${repoURL}`}>
-        <div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="sm:aspect-[7/8] aspect-square max-h-[70vh] w-full min-w-1/3 lg:px-10 px-14 py-10 max-w-[700px]
+        <div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="sm:aspect-[7/8] aspect-[10/9] max-h-[70vh] w-full min-w-1/3 lg:px-10 px-14 py-10 max-w-[700px]
         flex flex-col justify-evenly items-center space-y-5
-        rounded-xl border-neutral-900/60 xl:border-none
-        hover:cursor-pointer hover:bg-slate-900/60 lg:hover:scale-105 hover:scale-[101%] hover:scale-none hover:ease-out duration-300
-        font-normal bg-gradient-to-r from-neutral-900/10 to-slate-900/10">
+        sm:rounded-xl sm:border-neutral-900/30 sm:border border-b border-neutral-900/40
+        hover:cursor-pointer sm:hover:bg-slate-900/60 lg:hover:scale-105 sm:hover:scale-[101%] hover:ease-out duration-300
+        font-normal bg-gradient-to-r from-neutral-900/10 to-slate-700/5">
             <h2 className="text-center font-medium">{title}</h2>
             <AnimatePresence>
                 <div className={`guitar text-9xl ${hovered && "animate-prop"}`}>{emoji}</div>
