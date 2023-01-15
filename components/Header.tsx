f8fba90e3db22a7877fc58591ba4bc51db373b2c
Ryland Crouse <rmikhailc@gmail.com>
2023-01-15 11:16:54 -0500

    differed header links

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,10 +7,10 @@ const Header = (props: Props) => {
   return (
     <header className="flex px-th h-20 w-full items-center font-semibold leading-none overflow-x-scroll no-scrollbar
     bg-gradient-to-r from-zinc-700/20 to-slate-700/10 border-inherit border-b space-x-16">
-      <Link href="/blog">Home</Link>
-      <Link href="/blog">Showcase</Link>
+      <Link href="/">Home</Link>
+      <Link href="/work/">Showcase</Link>
       <Link href="/blog/a">Blog</Link>
-      <Link href="/blog">Info</Link>
+      <Link href="/info">Info</Link>
     </header>
   )
 }
