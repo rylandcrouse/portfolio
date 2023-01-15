59122e327630f3cde4fc417cc513c35bc093f826
Ryland Crouse <rmikhailc@gmail.com>
2023-01-14 19:44:39 -0500

    adjust blog spacing

--- screens/blog/BlogScreen.tsx
+++ screens/blog/BlogScreen.tsx
@@ -7,7 +7,7 @@ type Props = {}
 
 const BlogScreen = ({ frontMatter, markdownBody }: BlogPostProps) => {
   return (
-    <main className="min-h-screen w-full py-32">
+    <main className="min-h-screen w-full py-28">
         <div className='w-4/5 max-w-screen-sm prose mx-auto dark:prose-invert'>
             {frontMatter && <h1 className="mb-24 text-5xl">{frontMatter.title}</h1>}
             <div dangerouslySetInnerHTML={{ __html: md().render(markdownBody) }} />
