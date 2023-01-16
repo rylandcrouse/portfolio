fd5a93fca4a65d6bedefbd5173c0f488708a7b10
Ryland Crouse <rmikhailc@gmail.com>
2023-01-16 17:15:30 -0500

    altered hero

--- screens/home/sections/HeroSection.tsx
+++ screens/home/sections/HeroSection.tsx
@@ -5,12 +5,12 @@ type Props = {}
 
 const HeroSection = (props: Props) => {
   return (
-    <div className="min-h-[50vh] w-full flex 2xl:px-72 justify-start px-th justify-center font-bold
-    bg-gradient-to-r from-white/5 via-white/ to-white/5 bg-s text-neutral-200
+    <div className="min-h-[50vh] w-full flex 2xl:px-72 px-th justify-center font-bold
+    bg-gradient-to-r from-zinc-900/30 to-zinc-800/20 text-neutral-200
     ">
       <h1 className="self-center justify-self-center font-semibold !leading-tight 
       xl:text-4xl sm:text-[3vmax] text-3xl sm:my-16 mt-12"> 
-        “I leverage and expand upon digital solutions with code.”
+        “Leverage and network digital solutions with code.”
       </h1>
      </div>
   )
