36c2449433c527411d2af0f2225da4229ef27006
Ryland Crouse <rmikhailc@gmail.com>
2023-01-16 16:55:22 -0500

    changed styles

--- screens/home/sections/HeroSection.tsx
+++ screens/home/sections/HeroSection.tsx
@@ -1,13 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {}
 
 const HeroSection = (props: Props) => {
   return (
-    <div className="min-h-[50vh] w-full flex 2xl:px-72 2xl:justify-start px-th justify-center font-bold">
+    <div className="min-h-[50vh] w-full flex 2xl:px-72 xl:justify-start px-th justify-center font-bold
+    bg-gradient-to-r from-white/5 via-white/ to-white/5 bg-s text-neutral-200
+    ">
       <h1 className="self-center justify-self-center font-semibold !leading-tight 
-      xl:text-8xl sm:text-[4vmax] text-3xl sm:my-16 mt-12">
-        “Howdy <span className="blog-title-emoji">👋🏻</span>. I leverage and expand upon digital solutions with code.”
+      xl:text-8xl sm:text-[3vmax] text-3xl sm:my-16 mt-12"> 
+        “I leverage and expand upon digital solutions with code.”
       </h1>
      </div>
   )
