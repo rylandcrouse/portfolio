2b818e5b9075ddecb9b57241d34df94bfa1424a0
Ryland Crouse <rmikhailc@gmail.com>
2023-01-16 16:54:22 -0500

    responsive spacing

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ type Props = {}
 const Header = (props: Props) => {
   return (
     <header className="flex px-th h-20 w-full items-center font-semibold leading-none overflow-x-scroll no-scrollbar
-    bg-gradient-to-r from-zinc-700/20 to-slate-700/10 border-inherit border-b space-x-16">
+    bg-gradient-to-r from-zinc-700/20 to-slate-700/10 border-inherit border-b lg:space-x-28 space-x-16">
       <Link href="/">Home</Link>
       <Link href="/work/">Showcase</Link>
       <Link href="/blog/a">Blog</Link>
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Header';
+import SocialBanner from './Banner';
 
 interface LayoutProps {
     children: React.ReactNode;
