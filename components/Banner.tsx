310c92dd9c9d3df9e422b4b0396c96fa8ea55021
Ryland Crouse <rmikhailc@gmail.com>
2023-01-16 16:54:12 -0500

    added banner component

---
+++ components/Banner.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const SocialBanner = (props: Props) => {
+  return (
+    <h2 className="flex px-th h-14 w-full items-center justify-center font-semibold leading-none overflow-x-scroll no-scrollbar
+    bg-gradient-to-r from-zinc-700/20 to-slate-700/10 border-inherit text-white/90 space-x-16">
+      <div>
+      Ryland's Software Showcase
+        </div>
+    </h2>
+  )
+}
+
+export default SocialBanner
